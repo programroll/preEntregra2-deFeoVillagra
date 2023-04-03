26daf07d48fcaf2e4675e8d162b4ecd94ab5f82d
programroll <programroll@gmail.com>
2023-04-03 15:50:07 -0300

    sin total

--- main.js
+++ main.js
@@ -4,54 +4,114 @@ let contraseña = ":P"
 let ingresarUruario = prompt("Ingrese su usuario")
 let ingresarContraseña = prompt("Ingrese la contraseña")
 
-if (usuario === ingresarUruario && contraseña === ingresarContraseña) {
-    let mensajeInicial = "Bienvenido\n1* Ver lista de productos\n2* Ver el total\n0* para salir"
-    let mensajeSecundario = "1* Articulo A $500\n2* Articulo B $200\n3* Articulo C $600"
+let productos = [
+    {
+        id: 1,
+        categoria: "indumentaria",
+        nombre: "remera",
+        precio: 3000,
+        stock: 2
+    },
+    {
+        id: 2,
+        categoria: "indumentaria",
+        nombre: "vestido",
+        precio: 4000,
+        stock: 3
+    },
+    {
+        id: 3,
+        categoria: "accesorios",
+        nombre: "collar-hilo",
+        precio: 1500,
+        stock: 3
+    },
+    {
+        id: 4,
+        categoria: "accesorios",
+        nombre: "monedero",
+        precio: 2000,
+        stock: 2
+    },
+    {
+        id: 5,
+        categoria: "calzado",
+        nombre: "bota",
+        precio: 8000,
+        stock: 3
+    },
+]
 
-    let opcion
+if (usuario === ingresarUruario && contraseña === ingresarContraseña) {
+
+    let mensajeInicial = "Bienvenido\nElija una opción:\n1* Ver lista de productos\n2* Elegir una categoría\n3*Agregar al carrito\n4*Mi carrito\n0* para salir"
+
     let total = 0
+    let opcion
+    let carrito = []
+
     do {
-        opcion = pedirOpcion(mensajeInicial)
+        opcion = Number(prompt(mensajeInicial))
+
         switch (opcion) {
             case 1:
-                let producto = pedirOpcion(mensajeSecundario)
-                switch (producto) {
-                    case 1:
-                        total = total + 500
+                alert("Nuestros productos son: \n" + lista(productos))
+                
+                break
 
-                        break;
-                    case 2:
-                        total = total + 200
-                        break;
-                    case 3:
-                        total = total + 600
-                        break;
+            case 2:
+                let categoria = prompt("Nuetras categorias son: \n Indumentaria\n Accesorios\n Calzado\n Ingrese una categoria").toLowerCase()
 
-                    default:
-                        alert("Ingrese una opcion correcta")
-                        break;
+                let productosFiltrados = productos.filter(
+                    (producto) => producto.categoria === categoria
+                )
+
+                if (productosFiltrados.length === 0) {
+                    alert("No tenemos esa categoría");
+                } else {
+                    alert(
+                        "Lista de productos por categoria: " +
+                        categoria + "\n" + lista(productosFiltrados)
+                    );
+                    break
+                }
+            case 3:
+                let seleccion = prompt(
+                    "Seleccione qué producto quiere por el nombre\n" + lista(productos)
+                    )
+                let productoSeleccionado = productos.find (producto => producto.nombre === seleccion)
+
+                if (productoSeleccionado) {
+                    carrito.push(productoSeleccionado)
+                    alert (`Se agregó  "${seleccion}" al carrito` )
+                } else {
+                    alert ("No tenemos un artículo con ese nombre")
                 }
 
-                break;
-            case 2:
-                alert("El total a pagar es de $" + total + ".")
-                break;
+                break
+
+            case 4:
+                alert("Los productos seleccionados son: \n" + lista(carrito))
+                
+                break
 
             case 0:
                 alert("Gracias por visitarnos :)!")
-                break;
+                break
 
             default:
                 alert("Ingrese una opcion correcta")
-                break;
+                break
         }
-    } while (opcion !== 0 && opcion !== 3);
+    } while (opcion !== 0)
 
 }
 else {
     alert("Usuario incorrecto")
 }
 
-function pedirOpcion(mensaje) {
-    return Number(prompt(mensaje))
+function lista(productos) {
+    return productos.map(producto =>{
+        return`Nombre: ${producto.nombre}   $ ${producto.precio} `
+    }).join("\n")
 }
